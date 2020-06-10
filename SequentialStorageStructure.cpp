6285b3aa03f746a25bc3746b369b43da437aec88
EdwardChang5467 <3152245290@qq.com>
2020-06-10 21:26:01 +0800

    GetElem

---
+++ SequentialStorageStructure.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class test
+{
+private:
+    int a;
+
+public:
+    test(int a) { this->a = a; }
+    int getA() { return this->a; }
+};
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
